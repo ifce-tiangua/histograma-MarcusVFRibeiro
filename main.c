2502882b45e0829bac40f7ac26f4b785b8f41f71
MarcusVFRibeiro <viniciusxz12@outlook.com>
2025-02-18 15:04:02 -0300

    envio 3

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 void imprime_histograma(int *vendas[], int num_categorias);
 
 int main()
 {
   int nc;
-  scanf("%d",&nc);
+  scanf("%d", &nc);
 
-  int **vendas = (int**)malloc(sizeof(int*)*nc);
+  int **vendas = (int **)malloc(sizeof(int *) * nc);
 
-  for(int i = 0; i < nc; i++)
+  for (int i = 0; i < nc; i++)
   {
-    vendas[i] = (int*)malloc(sizeof(int));
+    vendas[i] = (int *)malloc(sizeof(int));
     scanf("%d", vendas[i]);
   }
 
   imprime_histograma(vendas, nc);
 
-  for(int i = 0; i < nc; i++)
+  for (int i = 0; i < nc; i++)
   {
     free(vendas[i]);
   }
@@ -27,15 +27,16 @@ int main()
 
 void imprime_histograma(int *vendas[], int num_categorias)
 {
-  for(int i = 0; i < num_categorias; i++)
+  for (int i = 0; i < num_categorias; i++)
   {
     int vc = *vendas[i];
     printf("%d ", vc);
 
-    for(int k = 0; k < vc; k++)
+    for (int k = 0; k < vc; k++)
     {
       printf("*");
     }
+
     printf("\n");
   }
 }
